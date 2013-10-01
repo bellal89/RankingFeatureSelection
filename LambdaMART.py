98b940debd7d7f30e94e6beda5b3eae87cd7781f
Alexander <bellal89@mail.ru>
2013-10-01 13:12:39 +0600

    Some changes...

--- LambdaMART.py
+++ LambdaMART.py
@@ -4,7 +4,7 @@ from FeatureVector import FeatureVector
 from RankFilter import RankFilter
 import matplotlib.pyplot as plt
 
-features_file_name = "RankLib/SolrTrain_57.txt"
+data_folder = "RankLib/Data"
 
 def parse(line):
 	parts = line.split('#')[0].strip().split(' ')
@@ -20,7 +20,36 @@ def clear_folder(folder):
 		except:
 			print("Cannot delete file " + full_file_name)
 
-def gen_plot(ranker):
+def gen_plot(features_file_name):
+
+	if os.path.exists(features_file_name + '.Weights'):
+		weights = [float(w.strip()) for w in open(features_file_name + '.Weights', 'r').readlines() if w.strip() != '']
+	else:
+		f_f = open(features_file_name, 'r')
+		feature_lines = f_f.readlines()
+		f_f.close()
+
+		feature_vectors = []
+		for line in feature_lines:
+			feature_vectors.append(parse(line))
+		rank = RankFilter(feature_vectors)
+		
+		weights = rank.filter(3)
+		
+		fw_f = open(features_file_name + '.Weights', 'w')
+		fw_f.write("\n".join([str(w) for w in weights]))
+		fw_f.close()
+
+	# print(weights)
+	sorted_weights = sorted([(i + 1, w) for i, w in enumerate(weights)], key=lambda t: t[1], reverse=True)
+
+	clear_folder("RankLib/SelectedFeatureSets")
+	for i in xrange(1, len(sorted_weights) + 1):
+		f = open("RankLib/SelectedFeatureSets/"+str(i), 'w')
+		for w in sorted_weights[0:i]:
+			f.write(str(w[0]) + '\n')
+		f.close()
+
 	clear_folder("RankLib/Results")
 	x = []
 	y = []
@@ -36,7 +65,7 @@ def gen_plot(ranker):
 	plt.title('Relief-like algorithm')
 	plt.xlabel('# of best features')
 	plt.ylabel('NDCG@10')
-	# plt.legend(str(ranker))
+	# plt.legend(features_file_name)
 	# Save the figure in a separate file
 	plt.savefig("RankLib/" + features_file_name + ".png")
 
@@ -48,34 +77,6 @@ def get_test_ndcg(features_count):
 	f.close()
 	return ndcg
 
-if os.path.exists(features_file_name + '.Weights'):
-	weights = [float(w.strip()) for w in open(features_file_name + '.Weights', 'r').readlines() if w.strip() != '']
-else:
-	f_f = open(features_file_name, 'r')
-	feature_lines = f_f.readlines()
-	f_f.close()
-
-	feature_vectors = []
-	for line in feature_lines:
-		feature_vectors.append(parse(line))
-	rank = RankFilter(feature_vectors)
-	
-	weights = rank.filter(3)
-	
-	fw_f = open(features_file_name + '.Weights', 'w')
-	fw_f.write("\n".join([str(w) for w in weights]))
-	fw_f.close()
-
-# print(weights)
-sorted_weights = sorted([(i + 1, w) for i, w in enumerate(weights)], key=lambda t: t[1], reverse=True)
-
-clear_folder("RankLib/SelectedFeatureSets")
-for i in xrange(1, len(sorted_weights) + 1):
-	f = open("RankLib/SelectedFeatureSets/"+str(i), 'w')
-	for w in sorted_weights[0:i]:
-		f.write(str(w[0]) + '\n')
-	f.close()
-
-ranker_list = [6]
-for ranker in ranker_list:
-	gen_plot(ranker)
+for file_name in os.listdir(data_folder):
+	full_file_name = os.path.join(data_folder, file_name)
+	gen_plot(full_file_name)
